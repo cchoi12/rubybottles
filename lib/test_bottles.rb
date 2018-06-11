f61a0d1c8a47c46bcfb61c0b0241803068a532d1
Christopher Choi <cdddchris@gmail.com>
2018-06-10 21:19:06 -0700

    Add additional refactoring

--- lib/test_bottles.rb
+++ lib/test_bottles.rb
@@ -13,13 +13,21 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = BottleNumber.new(number)
-    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    bottle_number = bottle_number_for(number)
+    next_bottle_number = bottle_number_for(bottle_number.successor)
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" \
     "#{bottle_number.action}" \
     "#{next_bottle_number} of beer on the wall.\n"
+  end
+
+  def bottle_number_for(number)
+    if number == 0
+      BottleNumber0
+    else
+      BottleNumber
+    end.new(number)
   end
 end
 
@@ -43,11 +51,7 @@ class BottleNumber
   end
 
   def quantity
-    if number.zero?
-      'no more'
-    else
-      number.to_s
-    end
+    number.to_s
   end
 
   def action
@@ -72,6 +76,12 @@ class BottleNumber
     else
       number - 1
     end
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  def quantity
+    'no more'
   end
 end
 

fa72e326ca1cf71d5baeb9a93cc6b28fc161a605
Christopher Choi <cdddchris@gmail.com>
2018-06-17 22:05:25 -0700

    Create objects for successor
    * Return objects instead of integers for successor method.
    * Move factory to BottleNumber class instead of being apart of verse.

--- lib/test_bottles.rb
+++ lib/test_bottles.rb
@@ -13,23 +13,12 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = bottle_number_for(number)
-    next_bottle_number = bottle_number_for(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" \
     "#{bottle_number.action}" \
-    "#{next_bottle_number} of beer on the wall.\n"
-  end
-
-  def bottle_number_for(number)
-    if number.zero?
-      BottleNumber0
-    elsif number == 1
-      BottleNumber1
-    else
-      BottleNumber
-    end.new(number)
+    "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
@@ -38,6 +27,17 @@ class BottleNumber
 
   def initialize(number)
     @number = number
+  end
+
+  def self.for(number)
+    case number
+    when 0
+      BottleNumber0
+    when 1
+      BottleNumber1
+    else
+      BottleNumber
+    end.new(number)
   end
 
   def to_s
@@ -61,7 +61,7 @@ class BottleNumber
   end
 
   def successor
-    number - 1
+    BottleNumber.for(number.pred)
   end
 end
 
@@ -85,7 +85,7 @@ class BottleNumber0 < BottleNumber
   end
 
   def successor
-    99
+    BottleNumber.for(99) 
   end
 end
 
